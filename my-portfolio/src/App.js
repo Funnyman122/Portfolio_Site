310273ab8587a87eaf8a3b028934fd4905c724dc
Funnyman <smithianman@gmail.com>
2024-01-30 09:43:05 +0000

    Update App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -16,11 +16,7 @@ function App() {
         <h1 style={{ color: 'green', textAlign: 'center' }}>NGC</h1>
         <p style={{ color: 'purple', textAlign: 'center' }}>The next generation is here</p>
       </div>
-      <CustomPlayer 
-        url="https://dl.dropboxusercontent.com/scl/fi/8vk934m5fftgt1stawsu9/spotifydown.com-DEMONS-IN-MY-SOUL.mp3?rlkey=crdr06f0p2vwhdyeragnju6h0"
-        thumbnail="https://i.ibb.co/9YxJD4x/Screenshot-20231119-120007.png"
-        title="DEMONS IN MY SOUL"
-      />
+  
     </div>
   );
 }
